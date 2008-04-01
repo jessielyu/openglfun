2fbce58c38621b603193feeb2d96b0c83d949402
NoEvilPeople <NoEvilPeople@b5d7a6e1-d349-0410-948e-53f8f13d1be3>
2008-04-01 19:07:19 +0000

    crap

--- Assignment One/Texture.h
+++
@@ -1,9 +0,0 @@
-#include <GLUT/glut.h>
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-
-class Texture
-{
-public:
-		static GLuint loadTexture(const char* filename, GLuint width, GLuint height);
-};
