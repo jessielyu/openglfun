527a56e4b7337a1380f91f49317f8769f210e8be
nospampeople <nospampeople@b5d7a6e1-d349-0410-948e-53f8f13d1be3>
2011-07-12 17:05:54 +0000

    Added array new and array delete. Got rid of compile warnings by adding throw declarations.  I hope declaring that these functions throw exceptions doesn't slow them down.

--- GameEngine/Main Project/Classes/Memory.h
+++ GameEngine/Main Project/Classes/Memory.h
@@ -9,6 +9,10 @@
 #include <new>  // To overload new and delete
 #include <cstdlib>  // For malloc and free
 
-void* operator new (size_t size);
+void* operator new (size_t size) throw(std::bad_alloc);
 
-void operator delete(void* p);
+void operator delete(void* p) throw();
+
+void* operator new[](size_t s) throw(std::bad_alloc);
+
+void operator delete[](void *dp) throw();
